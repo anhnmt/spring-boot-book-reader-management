3a029744a34e1ccdad8563cda71d060cd77cc25c
xdorro <xdorro@gmail.com>
2020-12-18 08:22:37 +0700

    Khởi tạo project

---
+++ src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+}
